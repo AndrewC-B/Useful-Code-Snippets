7c88a17066a06e4ff84430f437ef641238f2eed6
Andrew Curtis-Black <andrew.curtis-black@pg.canterbury.ac.nz>
2018-02-21 22:31:58 +1300

    Added timer code to fast_exp.

--- fast_exponentiation.py
+++ fast_exponentiation.py
@@ -1,3 +1,6 @@
+from time import time
+from functools import partial
+
 def fast_exp(a, b):
     if b < 0:
         return 1 / fast_exp(a, -b)
@@ -14,3 +17,23 @@ def fast_exp(a, b):
         counter <<= 1
     
     return ans
+    
+def naive(a, b):
+    total = a
+    for i in range(b-1):
+        total *= a
+    return total
+    
+def timer(function):
+    start = time()
+    function()
+    return time() - start
+
+a = 999
+b = 999
+naive = partial(naive, a, b)
+ours = partial(fast_exp, a, b)
+theirs = partial(pow, a, b)
+print(timer(naive))
+print(timer(ours))
+print(timer(theirs))
